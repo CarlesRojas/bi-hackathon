49a5177bc21c514a3ec9a24639aec8c2be41062f
marcgrau96 <m.grau@mundimoto.com>
2023-11-18 18:30:31 +0100

    add results endpoint

--- simon_back/backend/result.py
+++ simon_back/backend/result.py
@@ -1,22 +1,26 @@
-from typing import Dict, Any
+from datetime import datetime, timedelta, date
 
 from django.db.models import Q
 from django.http import HttpRequest, JsonResponse
 from django.views.decorators.http import require_http_methods
 
-from backend.models import Medication
+from backend.enums import EventType
+from backend.models import Medication, EventAssistance, Event, MedicationHistory
 
 
 @require_http_methods(["GET"])
 def get(request: HttpRequest, user_id: str) -> JsonResponse:
-    medications = Medication.objects.filter(Q(patient__id=user_id))
-    return JsonResponse(list(map(serialize, medications)), safe=False)
-
-
-def serialize(medication: Medication) -> Dict[str, Any]:
-    return {
-        "medication_days": medication.name,
-        "medication_taken": medication.hour,
-        "events_assisted": medication.created_at,
-        "appointments_assisted": medication.created_at,
-    }
+    today_day_of_week = datetime.now().isoweekday()
+    first_day_of_week = date.today() - timedelta(days=today_day_of_week)
+    medication_days = Medication.objects.filter(Q(patient__id=user_id)).count()*7
+    medication_taken = MedicationHistory.objects.filter(Q(medication__patient__id=user_id), Q(created_at__gt=first_day_of_week)).count()
+    events_assisted = Event.objects.filter(Q(id__in=EventAssistance.objects.values_list('event', flat=True)
+                                           .filter(Q(user__id=user_id))), Q(type=EventType.SOCIAL_EVENT.value), Q(start_date__gt=first_day_of_week), Q(start_date__lte=datetime.now())).count()
+    appointments_assisted = Event.objects.filter(Q(id__in=EventAssistance.objects.values_list('event', flat=True)
+                                                 .filter(Q(user__id=user_id))), Q(type=EventType.MEDICAL_APPOINTMENT.value), Q(start_date__gt=first_day_of_week), Q(start_date__lte=datetime.now())).count()
+    return JsonResponse({
+        "medication_times": medication_days,
+        "medication_taken": medication_taken,
+        "events_assisted": events_assisted,
+        "appointments_assisted": appointments_assisted,
+    })
